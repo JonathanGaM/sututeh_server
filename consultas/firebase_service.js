56612afdb968930b6e34b5ab095ad54f5098dd56
Jonathan Garcia <20221074@uthh.edu.mx>
2025-11-24 19:52:53 -0600

    Fix: nuevas variables Firebase en Render

--- consultas/firebase_service.js
+++ consultas/firebase_service.js
@@ -7,7 +7,7 @@ const pool = require('../bd');
 
 
 const serviceAccount = {
-  type: "service_account",
+  type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
@@ -17,7 +17,9 @@ const serviceAccount = {
   token_uri: process.env.FIREBASE_TOKEN_URI,
   auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
   client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN, // ⭐ IMPORTANTE
 };
+
 
 // Inicializar Firebase Admin
 if (!admin.apps.length) {
